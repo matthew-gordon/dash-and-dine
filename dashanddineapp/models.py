81930eb06653fbb4748b348913964e84e9a8c87a
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-27 20:37:03 -0700

    Add Customer, Driver model

--- dashanddineapp/models.py
+++ dashanddineapp/models.py
@@ -14,3 +14,21 @@ class Restaurant(models.Model):
     # Displays restaurant name in admin dashboard
     def __str__(self):
         return self.name
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
+    avatar = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500, blank=True)
+    address = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.get_full_name()
+
+class Driver(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='driver')
+    avatar = models.CharField(max_length=500)
+    phone = models.CharField(max_length=500, blank=True)
+    address = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.get_full_name()
