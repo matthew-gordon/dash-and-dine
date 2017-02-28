6fc5f139129d0ea1c3cd7ec100b8119b6e6a2fd3
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-28 14:31:48 -0700

    Add edit form function, edit form to account page

--- dashanddineapp/forms.py
+++ dashanddineapp/forms.py
@@ -6,12 +6,19 @@ from dashanddineapp.models import Restaurant
 class UserForm(forms.ModelForm):
     email = forms.CharField(max_length=100, required=True)
     password = forms.CharField(widget=forms.PasswordInput())
-    
+
     class Meta:
         model = User
-        fields = ["username", "password", "first_name", "last_name", "email"]
+        fields = ("username", "first_name", "last_name")
+
+class UserEditForm(forms.ModelForm):
+    email = forms.CharField(max_length=100, required=True)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email")
 
 class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
-        fields = ["name", "phone", "address", "logo"]
+        fields = ("name", "phone", "address", "logo")
--- dashanddineapp/views.py
+++ dashanddineapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from dashanddineapp.forms import UserForm, RestaurantForm
+
+from dashanddineapp.forms import UserForm, RestaurantForm, UserEditForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
@@ -10,11 +11,25 @@ def home(request):
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_home(request):
-    return render(request, 'restaurant/home.html', {})
+    return redirect(restaurant_order)
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_account(request):
-    return render(request, 'restaurant/account.html', {})
+    user_form = UserEditForm(instance=request.user)
+    restaurant_form = RestaurantForm(instance=request.user.restaurant)
+
+    if request.method == "POST":
+        user_form = UserEditForm(request.POST, instance=request.user)
+        restaurant_form = RestaurantForm(request.POST, request.FILES, instance=request.user.restaurant)
+
+        if user_form.is_valid() and restaurant_form.is_valid():
+            user_form.save()
+            restaurant_form.save()
+
+    return render(request, 'restaurant/account.html', {
+        "user_form": user_form,
+        "restaurant_form": restaurant_form
+    })
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_meal(request):
