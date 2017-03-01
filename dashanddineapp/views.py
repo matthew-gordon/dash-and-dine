1defc2ee4afe3b8043e33bbed2059c21943400a9
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-28 18:26:13 -0700

    Add meals list

--- dashanddineapp/views.py
+++ dashanddineapp/views.py
@@ -5,6 +5,8 @@ from dashanddineapp.forms import UserForm, RestaurantForm, UserEditForm, MealFor
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
+from dashanddineapp.models import Meal
+
 # Create your views here.
 def home(request):
     return redirect(restaurant_home)
@@ -33,7 +35,11 @@ def restaurant_account(request):
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_meal(request):
-    return render(request, 'restaurant/meal.html', {})
+    meals = Meal.objects.filter(restaurant=request.user.restaurant).order_by("-id")
+
+    return render(request, 'restaurant/meal.html', {
+        "meals": meals
+    })
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_add_meal(request):
