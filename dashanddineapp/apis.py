ba4d6c837e77de34c203538b31555239e1f43e62
Matthew Gordon <laxredemption5525@gmail.com>
2017-02-28 20:33:49 -0700

    Add absolute uri for logo url

--- dashanddineapp/apis.py
+++ dashanddineapp/apis.py
@@ -6,7 +6,8 @@ from dashanddineapp.serializers import RestaurantSerializer
 def customer_get_restaurant(request):
     restaurants = RestaurantSerializer(
         Restaurant.objects.all().order_by("-id"),
-        many = True
+        many = True,
+        context = {"request": request}
     ).data
 
     return JsonResponse({"restaurants": restaurants})
