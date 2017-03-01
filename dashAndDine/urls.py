1d0a723ab2dd41e655c5898be9b8a23cae724bf7
Matthew Gordon <laxredemption5525@gmail.com>
2017-03-01 13:33:24 -0700

    Add driver pick up order

--- dashAndDine/urls.py
+++ dashAndDine/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
 
     # APIs for Drivers
     url(r'^api/driver/orders/ready/$', apis.driver_get_ready_orders),
-    url(r'^api/driver/order/pickup/$', apis.driver_pick_up_order),
+    url(r'^api/driver/order/pick_up/$', apis.driver_pick_up_order),
     url(r'^api/driver/order/latest/$', apis.driver_get_latest_order),
     url(r'^api/driver/order/complete/$', apis.driver_complete_order),
     url(r'^api/driver/revenue/$', apis.driver_get_revenue),
